0c227830690e9fd8af4e7033df16c7d679a94381
David Mason <djmot@users.noreply.github.com>
2020-11-22 16:20:48 -0500

    add multi-browser (chrome + edge) testing

--- test/test.js
+++ test/test.js
@@ -4,39 +4,52 @@ var path = require('path');
 var assert = require('assert');
 var webdriver = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
-const { addConsoleHandler } = require('selenium-webdriver/lib/logging');
-// var edge = require('selenium-webdriver/edge');
+var edge = require('selenium-webdriver/edge');
 require('dotenv').load();
 
 var APP_URL = process.env['APP_URL'].slice(0, -1) + ':' + process.env['PORT'] + '/';
 
-var chromeDriverPath = path.join(__dirname, 'drivers', 'chromedriver.exe');
-var chromeService = new chrome.ServiceBuilder(chromeDriverPath);
+var browsers = ['chrome', 'MicrosoftEdge'];
+var driverFilenames = {
+    'chrome':           'chromedriver.exe',
+    'MicrosoftEdge':    'msedgedriver.exe'
+};
+var driverModules = {
+    'chrome':           chrome,
+    'MicrosoftEdge':    edge
+};
+var services = {};
+browsers.forEach((browser) => {
+    let driverPath = path.join(__dirname, 'drivers', driverFilenames[browser]);
+    services[browser] = new driverModules[browser].ServiceBuilder(driverPath);
+})
 
+browsers.forEach((browser) => {
+    describe('Basic Selenium tests (' + browser + ')', function () {
+        this.timeout(10000);
+        var driver;
 
-describe('Basic Selenium tests (Chrome)', function () {
-    this.timeout(10000);
-    var driver;
+        before(function () {
+            driver = new webdriver.Builder()
+                .forBrowser(browser)
+                .setChromeService(services['chrome'])
+                .setEdgeService(services['MicrosoftEdge'])
+                .build();
+        });
 
-    before(function () {
-        driver = new webdriver.Builder()
-            .forBrowser('chrome')
-            .setChromeService(chromeService)
-            .build();
-    });
+        after(function () {
+            driver.close();
+            return driver.quit();
+        });
 
-    after(function () {
-        driver.close();
-        return driver.quit();
-    });
+        it('should get home page', function () {
+            return driver.get(APP_URL);
+        });
 
-    it('should get home page', function () {
-        return driver.get(APP_URL);
-    });
-
-    it('should have correct home page title', function () {
-        return driver.getTitle().then(function(title) {
-            assert.strictEqual(title, 'nightlife-djmot');
+        it('should have correct home page title', function () {
+            return driver.getTitle().then(function (title) {
+                assert.strictEqual(title, 'nightlife-djmot');
+            });
         });
     });
-});
+})
