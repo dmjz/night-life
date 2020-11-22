d63509b9b138f7e75acaff19eccb748c4b462d75
David Mason <djmot@users.noreply.github.com>
2020-11-22 14:00:54 -0500

    add basic selenium test with mocha

--- test/test.js
+++ test/test.js
@@ -1,12 +1,42 @@
 'use strict';
 
+var path = require('path');
 var assert = require('assert');
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+const { addConsoleHandler } = require('selenium-webdriver/lib/logging');
+// var edge = require('selenium-webdriver/edge');
+require('dotenv').load();
 
-describe('Dummy Mocha tests', function () {
-    it('should return 2+2=4', function () {
-        assert.strictEqual(2+2, 4);
+var APP_URL = process.env['APP_URL'].slice(0, -1) + ':' + process.env['PORT'] + '/';
+
+var chromeDriverPath = path.join(__dirname, 'drivers', 'chromedriver.exe');
+var chromeService = new chrome.ServiceBuilder(chromeDriverPath);
+
+
+describe('Basic Selenium tests (Chrome)', function () {
+    this.timeout(10000);
+    var driver;
+
+    before(function () {
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .setChromeService(chromeService)
+            .build();
     });
-    it('should return 2+3=5', function () {
-        assert.strictEqual(2+3, 5);
+
+    after(function () {
+        driver.close();
+        return driver.quit();
+    });
+
+    it('should get home page', function () {
+        return driver.get(APP_URL);
+    });
+
+    it('should have correct home page title', function () {
+        return driver.getTitle().then(function(title) {
+            assert.strictEqual(title, 'nightlife-djmot');
+        });
     });
 });
